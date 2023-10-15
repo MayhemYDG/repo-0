dc026a92b3b89296c858424992f27fe1944670b4
Matthew Casperson <matthewcasperson@gmail.com>
2023-10-16 09:02:21 +1000

    Added generator

---
+++ generator-octopus-reference-architecture-apps/generators/nodejs-docker-webapp/index.js
@@ -0,0 +1,50 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        `Welcome to the breathtaking ${chalk.red(
+          "Octopus Reference Architecture Apps"
+        )} generator!`
+      )
+    );
+
+    const prompts = [
+      {
+        type: "confirm",
+        name: "someAnswer",
+        message: "Would you like to enable this option?",
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath("**/*"),
+      this.destinationPath(this.destinationRoot()),
+      null,
+      null,
+      {
+        globOptions: {
+          dot: true,
+          ignore: [this.templatePath("node_modules/**/*")]
+        }
+      }
+    );
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
