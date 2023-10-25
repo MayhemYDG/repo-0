84e1a63df7546bee95acd70fce91b18398763ab4
Matthew Casperson <matthewcasperson@gmail.com>
2023-10-25 15:59:23 +1000

    Added some more comments

--- generators/octopus-reference-architecture/index.js
+++ generators/octopus-reference-architecture/index.js
@@ -3,6 +3,34 @@ const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
 
+/*
+The command below is an example of how to run the generator:
+
+yo octopus-reference-architecture-apps:octopus-reference-architecture \
+  --force \
+  --skip-install \
+  --projectGroupName "Azure Web App" \
+  --infrastructureProjectName "_ Azure Web App Infrastructure" \
+  --infrastructureProjectDescription "Runbooks to create and manage Azure Web Apps" \
+  --infrastructureRunbookName "Create Web App" \
+  --infrastructureRunbookDescription "Creates an Azure web app and the associated Octopus target" \
+  --octopubFrontendProjectName "Azure WebApp Octopub Frontend" \
+  --octopubFrontendProjectDescription "Deploys the Octopub Frontend app to an Azure Web App target" \
+  --octopubProductsProjectName "Azure WebApp Octopub Products" \
+  --octopubProductsProjectDescription "Deploys the Octopub Products app to an Azure Web App target" \
+  --octopubAuditsProjectName "Azure WebApp Octopub Audits" \
+  --octopubAuditsProjectDescription "Deploys the Octopub Audits app to an Azure Web App target" \
+  --octopubOrchestrationProjectName "_ Deploy Azure Web App Octopub Stack" \
+  --octopubOrchestrationProjectDescription "Deploys the full Octopub application stack" \
+  --frontendHealthCheck "/index.html" \
+  --productsHealthCheck "/health/products" \
+  --auditsHealthCheck "/health/audits" \
+  --smokeTestContainerImage "octopuslabs/azure-workertools" \
+  --targetRole "Azure_Web_App_Reference_Architecture" \
+  --smokeTestActionType "Octopus.AzurePowerShell" \
+  --feedbackLink "https://octopus.com"
+ */
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
@@ -44,15 +72,18 @@ module.exports = class extends Generator {
     });
     this.option("frontendHealthCheck", {
       type: String,
-      required: true
+      required: true,
+      default: "/index.html"
     });
     this.option("productsHealthCheck", {
       type: String,
-      required: true
+      required: true,
+      default: "/health/products"
     });
     this.option("auditsHealthCheck", {
       type: String,
-      required: true
+      required: true,
+      default: "/health/audits"
     });
     this.option("smokeTestContainerImage", {
       type: String,
