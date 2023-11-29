214643424f7523675c9a5e8db3a6f66834478775
Matthew Casperson <matthewcasperson@gmail.com>
2023-11-30 09:36:14 +1000

    Removed old variable sets

--- generators/octopus-reference-architecture/templates/merge.sh
+++ generators/octopus-reference-architecture/templates/merge.sh
@@ -5,7 +5,6 @@ cat locals.tf >> template.tf
 cat library_variable_set.tf >> template.tf
 cat environments.tf >> template.tf
 cat feeds.tf >> template.tf
-cat library_variable_sets.tf >> template.tf
 cat accounts.tf >> template.tf
 cat worker_pools.tf >> template.tf
 cat project_groups.tf >> template.tf
