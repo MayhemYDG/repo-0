56607d4acad970df9dda2825152ecb05abac9072
Matthew Casperson <matthewcasperson@gmail.com>
2023-11-30 10:24:12 +1000

    Add the docker lib var set

--- generators/octopus-reference-architecture/templates/library_variable_set.tf
+++ generators/octopus-reference-architecture/templates/library_variable_set.tf
@@ -4,15 +4,26 @@
 # accounts, feeds, certificates etc to be recreated with octoterra.
 # Note these variables are not consumed by the "regular" runbooks or deployment steps.
 
-data "octopusdeploy_library_variable_sets" "space_resource_secrets" {
-  partial_name = "Octopus Space Resources"
+data "octopusdeploy_library_variable_sets" "docker" {
+  partial_name = "Docker"
   skip         = 0
   take         = 1
 }
 
-resource "octopusdeploy_library_variable_set" "octopus_library_variable_set" {
-  count       = length(data.octopusdeploy_library_variable_sets.space_resource_secrets.library_variable_sets) == 0 ? 1 : 0
-  name        = "Octopus Space Resources"
-  description = "The details of space level resources, used when recreating the space with octoterra"
+resource "octopusdeploy_library_variable_set" "docker" {
+  count       = length(data.octopusdeploy_library_variable_sets.docker.library_variable_sets) == 0 ? 1 : 0
+  name        = "Docker"
+  description = "Credentials used to interact with Docker"
+}
+
+resource "octopusdeploy_variable" "docker_username" {
+  count        = length(data.octopusdeploy_library_variable_sets.docker.library_variable_sets) == 0 ? 1 : 0
+  name         = "Docker.Credentials.Username"
+  type         = "String"
+  description  = "The Docker username"
+  is_sensitive = false
+  is_editable  = true
+  owner_id     = octopusdeploy_library_variable_set.docker[0].id
+  value        = var.feed_docker_hub_username
 }
 #endregion
